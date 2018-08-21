5549b642b9c83531110779f2177912ace4ea2d2a
Doma <leishenghao@126.com>
2018-08-21 20:08:27 +0800

    refactor(api): add support for caching with ETag

--- github/src/index.js
+++ github/src/index.js
@@ -17,5 +17,35 @@ axios.defaults.headers = {
   Authorization: `token ${require('./access-token')}`
 };
 
+// 304 caching
+
+axios.defaults.validateStatus = function(status) {
+  return status >= 200 && status < 300 || status === 304;
+};
+
+const url2ETagMap = {};
+const eTag2DataMap = {};
+
+axios.interceptors.request.use(function(config) {
+  if (url2ETagMap[config.url]) {
+    config.headers['If-None-Match'] = url2ETagMap[config.url];
+  }
+  return config;
+});
+
+axios.interceptors.response.use(function(response) {
+  if (response.status === 304) {
+    return {
+      ...response,
+      data: eTag2DataMap[response.config.headers['If-None-Match']]
+    };
+  }
+
+  if (response.status >= 200 && response.status < 300) {
+    url2ETagMap[response.config.url.replace(response.config.baseURL, '')] = response.headers.etag;
+    eTag2DataMap[response.headers.etag] = response.data;
+  }
+  return response;
+});
 // bootstrap React App
 ReactDOM.render(<App />, document.getElementById('root'));
